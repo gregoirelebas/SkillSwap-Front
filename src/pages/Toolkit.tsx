96654ec612aba6af137ccc584f021f6af0714ec6
Gregoire Lebas <gregoire.lebas@gmail.com>
2026-01-19 10:55:51 +0100

    Moved Toolkit in a separate page.

---
+++ src/pages/Toolkit.tsx
@@ -0,0 +1,15 @@
+export default function Toolkit() {
+  return (
+    <div className="card flex flex-col items-center justify-center gap-10 m-20">
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut blanditiis labore
+        soluta ipsum, iusto cumque iste tenetur dolorum eos ea repellendus eius quas? Libero
+        perferendis distinctio optio qui. Magni?
+      </p>
+    </div>
+  );
+}
