d79ca3fadce6094c3227acf9b3ace0ffeb7c2916
Gregoire Lebas <gregoire.lebas@gmail.com>
2026-01-22 10:26:01 +0100

    Modified css to remove additional files.

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,24 +1,25 @@
-import '../styles/input.css';
-
-import React from 'react';
+import type { InputHTMLAttributes } from 'react';
 
 interface InputProps {
   label?: string;
   isValid: boolean;
   error?: string;
-  inputProps: React.InputHTMLAttributes<HTMLInputElement>;
+  inputProps: InputHTMLAttributes<HTMLInputElement>;
 }
 
 export default function Input({ label, isValid, error, inputProps }: InputProps) {
   return (
     <div className="w-full column">
       {label && (
-        <label htmlFor={inputProps.id} className="input-label">
+        <label htmlFor={inputProps.id} className="w-full mb-4 pl-1 font-normal">
           {label}
         </label>
       )}
-      <input className={`input ${isValid ? '' : 'input-invalid'}`} {...inputProps} />
-      {!isValid && <span className="input-error">{error}</span>}
+      <input
+        className={`w-full p-3 bg-neutral-50 border ${isValid ? 'border-neutral-200' : 'border-danger'} rounded-lg focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent transition-all`}
+        {...inputProps}
+      />
+      {!isValid && <span className="w-full mt-1 pl-3 text-danger">{error}</span>}
     </div>
   );
 }
